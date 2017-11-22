595b75d0db0fbb899591b860464c589fb1ac681a
Seb <Seb.Parfitt@me.com>
2017-11-22 17:17:02 +0100

    NYAN NYAN NYAN NYAN NYAN NYAN

---
+++ Assets/Scripts/basicBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicBullet : MonoBehaviour {
+
+	Rigidbody rigidBody;
+	public float speed;
+	public float reloadTime;
+
+	void Awake(){
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void Start () {
+		rigidBody.velocity = new Vector3 (0, 0, speed);
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class player : MonoBehaviour {
     [Range(0,1)]
     public float movementSpeed;
+	public CurrentWeapon currentWeapon;
+	public float reloadTime;
 
-	// Use this for initialization
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	void Update (){
+		if (Input.GetAxis ("Fire1")>0 && reloadTime <= Time.time) {
+			Fire ();
+		}
+	}
+
+	void Fire(){
+		switch (currentWeapon) {
+		case (CurrentWeapon.Texture):
+			GameObject bullet = Resources.Load ("Weapons/TexNotFound") as GameObject;
+			reloadTime = Time.time + bullet.GetComponent<basicBullet> ().reloadTime;
+			Instantiate (bullet,transform.position,bullet.transform.rotation);
+			break;
+		}
+	}
+
 	void FixedUpdate () {
         transform.position += new Vector3(Input.GetAxis("Horizontal")*movementSpeed, 
                                           Input.GetAxis("Vertical")*movementSpeed,0);
 		
 	}
 }
+
+public enum CurrentWeapon {Texture, Other};
