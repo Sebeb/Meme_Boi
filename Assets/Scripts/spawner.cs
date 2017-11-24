31e74389939694230e5efdad94c0451ccb11fc71
Seb <Seb.Parfitt@me.com>
2017-11-25 00:48:40 +0100

    Began Implamenting Spawn Probability System
    Not functional yet

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -7,20 +7,42 @@ public class spawner : MonoBehaviour {
     public float lightSpawnTime;
     float nextLightSpawn;
     public GameObject[] enemyPool;
+	public List<float> SpawnQueue;
+	public List<GameObject> SpawnSchedule;
 
 
 	void Start () {
         game.controller.spawnZ = transform.position.z;
         enemyPool = Resources.LoadAll<GameObject>("Enemies/");
+		PopulateSpawnList ();
+	}
+
+	void PopulateSpawnList(){
+		foreach (GameObject enemy in enemyPool) {
+			
+		}
+	}
+
+	void AddEnemyToQueue(GameObject enemy){
+		spawnStats schedule = enemy.GetComponent<spawnStats> ();
+		float spawnTime = Random.Range (Time.time + schedule.spawnTimeMin, Time.time + schedule.spawnTimeMax);
+		if (SpawnQueue.Count != 0) {
+			for (int position = 0; position < SpawnQueue.Count; position++) {
+				if (SpawnQueue[position]>spawnTime;
+			}
+			
+		} else {
+			SpawnQueue.Add (enemy);
+			SpawnSchedule.Add (spawnTime);
+		}
 	}
 
     void FixedUpdate()
     {
-
         #region Light Spawn
         if (Time.fixedTime>nextLightSpawn){
             nextLightSpawn += lightSpawnTime;
-            GameObject light = Instantiate(Resources.Load("Light", typeof(GameObject)),transform.position,Quaternion.identity)as GameObject;
+            GameObject light = Instantiate(Resources.Load("Light", typeof(GameObject)),transform.position,Quaternion.identity) as GameObject;
             light.transform.parent = game.controller.tunnel.transform;
         }
 #endregion
--- Assets/spawnStats.cs
+++ Assets/spawnStats.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class spawnStats : MonoBehaviour {
-	int[] spawnProbability;
-
-	void Awake () {
-		spawnProbability = new int[game.controller.levels + 1];	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public float[] spawnTimeMin;
+	public float[] spawnTimeMax;
 }
