9188f0ee233440a713f442644ac2bd65452ee58d
Sebebstian <Seb.Parfitt@me.com>
2017-11-26 01:58:40 +0100

    Added mouse controls for max precision
    PC MASTER RACE 4 LYF

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -9,6 +9,7 @@ public class player : MonoBehaviour
     public Weapons currentWeapon;
     float reloadTime;
     public sfxManager fireSFX;
+    public bool mouseControl;
 
     private void OnTriggerEnter(Collider enemyCollider)
     {
@@ -62,11 +63,33 @@ public class player : MonoBehaviour
 
     void FixedUpdate()
     {
-		#region Movement
-        transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x + Input.GetAxis("Horizontal") * movementSpeed, -game.controller.rightBound, game.controller.rightBound),
-            Mathf.Clamp(transform.position.y + Input.GetAxis("Vertical") * movementSpeed, -game.controller.upperBound, game.controller.upperBound), 0);
-		#endregion
+        if (Input.mousePosition.x > 0 && Input.mousePosition.x < Screen.height * 2 && Input.mousePosition.y > 0 && Input.mousePosition.y < Screen.height * 2)
+        { if(Input.GetMouseButtonDown(0))
+            mouseControl = true;
+        }
+        else
+            mouseControl = false;
+        if (Input.GetAxis("Horizontal")!=0||
+            Input.GetAxis("Vertical") != 0)
+            mouseControl = false;
+            
+        
+        #region Movement
+        if (!mouseControl)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x + Input.GetAxis("Horizontal") * movementSpeed, -game.controller.rightBound, game.controller.rightBound),
+                Mathf.Clamp(transform.position.y + Input.GetAxis("Vertical") * movementSpeed, -game.controller.upperBound, game.controller.upperBound), 0);
+        }
+        else{
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Confined;
+            transform.position = new Vector3(Mathf.Clamp(-game.controller.rightBound + ((Input.mousePosition.x / Screen.width)) * game.controller.rightBound*2,-game.controller.rightBound,game.controller.rightBound),
+                                             Mathf .Clamp(-game.controller.upperBound + ((Input.mousePosition.y / Screen.height)) * game.controller.upperBound*2,-game.controller.upperBound,game.controller.upperBound),0);
+        }
+            #endregion
     }
 }
 
